9e554bb486fa86cbbebab5e35dec3dc0910263cf
Heneros <punk60826@gmail.com>
2022-08-03 23:55:03 +0300

    menu.js created

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,2 @@
-"use strict";
+"use strict";var btnLink=document.querySelector(".js-select-head");btnLink.addEventListener("click",(function(){}));
 //# sourceMappingURL=main.js.map
---
+++ src/js/components/menu.js
@@ -0,0 +1,5 @@
+const btnLink = document.querySelector('.js-select-head');
+
+btnLink.addEventListener('click', function () {
+
+});
