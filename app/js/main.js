9057e5b6cded9bce8072783fa76307d9588fa507
Heneros <punk60826@gmail.com>
2022-08-04 20:39:04 +0300

    menu js done

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,2 @@
-"use strict";var btnLink=document.querySelectorAll(".js-select"),menuOpen=document.querySelector(".select-header__main");
+"use strict";var btnLink=document.querySelectorAll(".js-select"),menuOpen=document.querySelector(".select-header__main");btnLink.forEach((function(e){e.addEventListener("click",(function(){menuOpen.classList.toggle("select-header__visible")}))}));
 //# sourceMappingURL=main.js.map
--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,3 +1,9 @@
 const btnLink = document.querySelectorAll('.js-select');
 const menuOpen = document.querySelector('.select-header__main');
 
+
+btnLink.forEach(el => {
+    el.addEventListener('click', () =>{
+        menuOpen.classList.toggle('select-header__visible');
+    })
+})
