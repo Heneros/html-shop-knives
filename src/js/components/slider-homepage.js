c2097999b775ad2c9ca8317354dcc79e6cac681c
Heneros <punk60826@gmail.com>
2022-08-12 01:03:47 +0300

    updated slider-homepage

--- src/js/components/slider-homepage.js
+++ src/js/components/slider-homepage.js
@@ -9,14 +9,18 @@ const swiper = new Swiper('.slider-homepage__swiper', {
             let indT = total >= 10 ? total : `0${total}`
             let indC = current >= 10 ? current : `0${current}`
             return `<b>${indC}</b><span></span> ${indT}`
+                +
+                `<span class='swiper-pagination-bullet'></span>`
+
+
         }
     },
-    scrollbar:{
-        el: '.slider-homepage__scrollbar'
-    }
+    // scrollbar:{
+    //     el: '.slider-homepage__scrollbar'
+    // }
 
 });
-
+// `<span class='swiper-pagination-bullet ${indC}: swiper-pagination-bullet-active'></span>`
 
 ///Add span
 const slider__heading = document.querySelectorAll(".slider-homepage__title");
