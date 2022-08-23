994a2c4453ba9499f50b022454953599edbac06d
Heneros <punk60826@gmail.com>
2022-08-23 23:21:41 +0300

    new section novelty

--- src/js/components/slider-products.js
+++ src/js/components/slider-products.js
@@ -1,5 +1,8 @@
 const ProductsSlider = new Swiper('.bestsellers-products__swiper', {
     speed: 800,
     spaceBetween: 50,
-    slidesPerView: 4
+    slidesPerView: 4,
+    pagination: {
+        el: '.bestsellers-products__pagination'
+    }
 });
