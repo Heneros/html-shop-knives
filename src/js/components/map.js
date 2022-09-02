e17f0282625ef1511c426b08ebd128a904084093
Heneros <punk60826@gmail.com>
2022-09-02 21:41:29 +0300

    map.js created

---
+++ src/js/components/map.js
@@ -0,0 +1,26 @@
+const findMyState = () => {
+    const status = document.querySelector('.location');
+
+    const success = (position) => {
+        // console.log(position);
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        // console.log('latitude ' + latitude + ' longitude ' + longitude);
+
+        const geoApiUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude==${longitude}&localityLanguage=en`;
+
+        fetch(geoApiUrl)
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                status.textContent = data.principalSubdivision;
+            })
+    }
+
+    const error = () => {
+        status.textContent = "Kyiv";
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error);
+}
+findMyState();
