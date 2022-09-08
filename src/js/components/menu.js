4c18ff1afd837e68795c4e9dea3e9295c20ab977
Heneros <punk60826@gmail.com>
2022-09-08 23:36:35 +0300

    burger menu

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -51,13 +51,11 @@ window.addEventListener('scroll', function () {
 
 
 
-// document.querySelector(".js-burger").addEventListener('click', (e) => {
-//     if (this.closest('.js-burger-block').classList.contains("open")) {
-//         this.closest('.js-burger-block').classList.remove("open");
-//         this.closest('.js-burger-block').find('.burger__body').slideUp()
+const menu_btn = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
 
-//     } else {
-//         this.closest('.js-burger-block').classList.add("open");
-//         this.closest('.js-burger-block').find('.burger__body').slideDown()
-//     }
-// });
+menu_btn.addEventListener('click', function () {
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+    // alert(123);
+});
