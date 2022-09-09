2bea363fb0736d57902edee87e5417cf1407c066
Heneros <punk60826@gmail.com>
2022-09-10 00:07:10 +0300

    header mobile updated

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -54,8 +54,13 @@ window.addEventListener('scroll', function () {
 const menu_btn = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.mobile-nav');
 
+const header__logo = document.querySelector('.header__logo');
+const header__search = document.querySelector('.header__search-field');
+
+
 menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
-    // alert(123);
+    header__logo.classList.toggle('hidden');
+    header__search.classList.toggle('visible');
 });
